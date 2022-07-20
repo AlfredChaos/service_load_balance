9dc9790ae8553a3a1d404d4c7c4b96f6355fa55c
AlfredChaos <Caojie_the42@outlook.com>
2022-07-20 11:30:21 +0800

    add reverse proxy

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"myslb/requester"
+	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
 	r := InitRouters()
-	if err := r.Run(":33802"); err != nil {
+	if err := r.Run(":3380"); err != nil {
 		fmt.Println("run gin app error")
 		fmt.Println(err)
 	}
@@ -19,12 +21,27 @@ func InitRouters() *gin.Engine {
 	ver := fmt.Sprintf("/%s", "v1")
 	routerGroupVer := router.Group(ver)
 
-	routerGroupVer.GET("/cgi", Handler(&requester.GetCGIHandler{}))
+	//routerGroupVer.GET("/cgi", Handler(&requester.GetCGIHandler{}))
+	routerGroupVer.GET("/:name", ReverseProxy())
 	return router
 }
 
 func Handler(r requester.Requester) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requester.Handler(r, c)
+	}
+}
+
+func ReverseProxy() gin.HandlerFunc {
+	target := "127.0.0.1:33802"
+	return func(c *gin.Context) {
+		director := func(req *http.Request) {
+			req.URL.Scheme = "http"
+			req.URL.Host = target
+			req.Host = target
+		}
+		fmt.Println("Proxy here")
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.ServeHTTP(c.Writer, c.Request)
 	}
 }

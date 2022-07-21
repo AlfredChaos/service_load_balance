20cdcdc2cbbe86ff1d099c57be04806320642132
AlfredChaos <Caojie_the42@outlook.com>
2022-07-21 22:23:00 +0800

    round robin

--- constant/hosts.go
+++ constant/hosts.go
@@ -5,3 +5,17 @@ var Hosts = [...]string{
 	"127.0.0.1:30082",
 	"127.0.0.1:30083",
 }
+
+// server status
+const (
+	NetworkUnreachable = iota
+	ServerActive
+	ServerError
+	LoadTooHigh
+)
+
+// service status
+const (
+	ServiceActive = iota
+	ServiceError
+)
--- loadbalance/roundRobin.go
+++ loadbalance/roundRobin.go
@@ -1,32 +1,144 @@
 package loadbalance
 
-import "myslb/constant"
+import (
+	"errors"
+	"fmt"
+	"myslb/constant"
+	"net/http"
+	"sync"
+)
 
-type Queue struct {
-	Hosts  []string
-	Front  int
-	Tail   int
+type Backend struct {
+	Host          string
+	ServerStatus  int
+	ServiceStatus int
+}
+
+type RRPool struct {
+	Hosts  []*Backend
+	Index  int
 	Length int
+	Mutex  sync.RWMutex
 }
 
-func NewQueue() (q *Queue) {
+func NewRRPool() (q *RRPool) {
+	q = &RRPool{}
+	q.Mutex.Lock()
+	defer q.Mutex.Unlock()
+	for _, host := range constant.Hosts {
+		b := &Backend{Host: host}
+		if err := checkServiceStatus(host); err != nil {
+			b.ServiceStatus = constant.ServiceError
+		} else {
+			b.ServiceStatus = constant.ServiceActive
+		}
+		if status, err := checkServerStatus(host); err != nil {
+			fmt.Printf("check found %s status get error\n", host)
+			b.ServerStatus = status
+		} else {
+			b.ServerStatus = constant.ServerActive
+		}
+		q.Hosts = append(q.Hosts, b)
+		q.Length++
+	}
+	return q
+}
+
+func (q *RRPool) Enqueue(host string) {
+	q.Mutex.Lock()
+	defer q.Mutex.Unlock()
+	b := &Backend{Host: host}
+	if err := checkServiceStatus(host); err != nil {
+		b.ServiceStatus = constant.ServiceError
+	} else {
+		b.ServiceStatus = constant.ServiceActive
+	}
+	if status, err := checkServerStatus(host); err != nil {
+		fmt.Printf("check found %s status get error\n", host)
+		b.ServerStatus = status
+	} else {
+		b.ServerStatus = constant.ServerActive
+	}
+	q.Hosts = append(q.Hosts, b)
+	q.Length++
+}
+
+func (q *RRPool) Dequeue(host string) {
+	q.Mutex.Lock()
+	defer q.Mutex.Unlock()
+	for location, backend := range q.Hosts {
+		if backend.Host == host {
+			q.Hosts = append(q.Hosts[:location], q.Hosts[location+1:]...)
+		}
+		q.Length--
+	}
+}
+
+func checkServerStatus(host string) (status int, err error) {
 
 }
 
-func checkServerStatus(host string) (err error){
+func checkServiceStatus(host string) (err error) {
 
 }
 
-type RoundRobin struct{}
+type RoundRobin struct{
+	ServerPool *RRPool
+}
 
 func (r *RoundRobin) ServerAdd(host string) (err error) {
+	if r.ServerPool == nil {
+		r.ServerPool = NewRRPool()
+	}
+	r.ServerPool.Mutex.Lock()
+	r.ServerPool.Mutex.Unlock()
+	for _, backend := range r.ServerPool.Hosts {
+		if backend.Host == host {
+			fmt.Printf("host %s has already stay in pool\n", host)
+			return nil
+		}
+	}
+	r.ServerPool.Enqueue(host)
 	return nil
 }
 
 func (r *RoundRobin) ServerRemove(host string) (err error) {
+	if r.ServerPool == nil {
+		fmt.Println("pool has been empty")
+		return nil
+	}
+	r.ServerPool.Mutex.Lock()
+	defer r.ServerPool.Mutex.Unlock()
+	for _, backend := range r.ServerPool.Hosts {
+		if backend.Host == host {
+			r.ServerPool.Dequeue(host)
+			return nil
+		}
+	}
+	fmt.Printf("host %s not stay in pool\n", host)
 	return nil
 }
 
 func (r *RoundRobin) GetNextPeer() (host string, err error) {
-	return "", nil
+	if r.ServerPool == nil {
+		fmt.Println("pool has been empty")
+		return "", errors.New("pool has been empty")
+	}
+	r.ServerPool.Mutex.Lock()
+	defer r.ServerPool.Mutex.Unlock()
+	backend := r.getNextBackend()
+	for backend.ServiceStatus != constant.ServiceActive &&
+		backend.ServerStatus != constant.ServerActive {
+		backend = r.getNextBackend()
+	}
+	return backend.Host, nil
+}
+
+func (r *RoundRobin) getNextBackend() (backend *Backend) {
+	backend = r.ServerPool.Hosts[r.ServerPool.Index]
+	r.ServerPool.Index++
+	if r.ServerPool.Index == r.ServerPool.Length {
+		r.ServerPool.Index = 0
+	}
+	return backend
 }
